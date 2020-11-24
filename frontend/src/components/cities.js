4a03d356ef9342df082abb6c3dc6b993b0b8a7f3
Baptiste LECHAT <baptiste.lechat@ynov.com>
2020-11-24 21:13:15 +0100

    🐛✨ add scrollbar when result list is too long

--- frontend/src/components/cities.js
+++ frontend/src/components/cities.js
@@ -5,8 +5,18 @@ import ListItemText from '@material-ui/core/ListItemText';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import SearchIcon from '@material-ui/icons/Search';
+import ScrollArea from 'react-scrollbar'
+import { makeStyles } from '@material-ui/core/styles';
 
-function cities(props) {
+const useStyles = makeStyles((theme) => ({
+  scrollbar: {
+    height: "60vh",
+  },
+}));
+
+function Cities(props) {
+  
+  const classes = useStyles();
   
     return (
       <div>
@@ -14,18 +24,20 @@ function cities(props) {
           <ListItemIcon><SearchIcon color="secondary" fontSize='large'/></ListItemIcon>
           <h2>Résultat de votre recherche</h2>
         </ListItem>
-        <List>
-          { props.data.map((cities, index) => 
-          <ListItem button key={index}>
-            <ListItemIcon>
-              <ChevronRightIcon />
-            </ListItemIcon>
-            <ListItemText primary={cities.Code_postal ? cities.Nom_commune : "Aucune valeur correspondante à votre recherche"} secondary={`Code postal : ${cities.Code_postal} - INSEE : ${cities.Code_commune_INSEE} - Vent : ${cities.Vent} - Neige : ${cities.Neige} - Sismique : ${cities.Seisme}`}/>
-          </ListItem>)}
-        </List>
+        <ScrollArea className={classes.scrollbar}>
+          <List>
+            { props.data.map((cities, index) => 
+            <ListItem button key={index}>
+              <ListItemIcon>
+                <ChevronRightIcon />
+              </ListItemIcon>
+              <ListItemText primary={cities.Code_postal ? cities.Nom_commune : "Aucune valeur correspondante à votre recherche"} secondary={`Code postal : ${cities.Code_postal} - INSEE : ${cities.Code_commune_INSEE} - Vent : ${cities.Vent} - Neige : ${cities.Neige} - Sismique : ${cities.Seisme}`}/>
+            </ListItem>)}
+          </List>
+        </ScrollArea>
       </div>
     );
 }
 
-export default cities;
+export default Cities;
 
